e81c3838a6a299664ad090f984a3332c032f3cc2
David Chu <thedavidchu@gmail.com>
2021-07-16 22:42:23 -0400

    Added tkinter plot

--- Preprocessing/plot.py
+++ Preprocessing/plot.py
@@ -1,19 +1,33 @@
 import cv2
+import tkinter as tk
 
 
-def cv2_imshow(file_path: str):
+def cv2_imshow(image_path: str):
     """
     The image is height*width*channel.
 
-    :param file_path:
+    :param image_path:
     :return:
     """
-    img = cv2.imread(file_path)
-    cv2.imshow(file_path, img)
+    img = cv2.imread(image_path)
+    cv2.imshow(image_path, img)
     return
 
 
+def tkinter_imshow(image_path: str):
+    window = tk.Tk()
+
+    # Open Image
+    image = tk.PhotoImage(file=image_path)
+    window.title(image_path)
+    background_image = tk.Label(image=image)
+    background_image.pack()
+
+    tk.mainloop()
+
 if __name__ == '__main__':
-    cv2_imshow('images/skule/nebu.png')
+    image_path = '../images/skule/nebu.png'
+    # cv2_imshow(image_path)
+    tkinter_imshow(image_path)
 
 
