519c23568961b43ffa8bd5732f6b4d7be3712039
David Chu <thedavidchu@gmail.com>
2021-07-21 22:01:35 -0400

    Added file filtering function and made the directory search more general.

--- Preprocessing/server/path.py
+++ Preprocessing/server/path.py
@@ -12,16 +12,26 @@ def filter_image_paths(image_paths: str):
     )
 
 
-def get_image_paths(dir_path: str):
+def filter_label_paths(label_paths: str):
+    return list(
+        filter(
+            lambda path: re.match(r'^.*\.(txt|json|csv|tsv)$', path.lower()),
+            label_paths
+        )
+    )
+
+
+def get_paths(dir_path: str, filter_fnc):
     file_paths = []
     for dir_or_file in os.scandir(dir_path):
         if os.path.isdir(dir_or_file.path):
-            file_paths.extend(get_image_paths(dir_or_file.path))
+            file_paths.extend(get_paths(dir_or_file.path, filter_fnc))
         else:
             file_paths.append(dir_or_file.path)
 
-    return filter_image_paths(file_paths)
+    return filter_fnc(file_paths)
+
 
 
 if __name__ == '__main__':
-    image_paths = get_image_paths('../images/')
+    image_paths = get_paths('../../images/', filter_image_paths)
