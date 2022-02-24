645d1f85f503e00ba5958b6d242a9bfdb3f57718
Ammar Vora <ammarmust4@gmail.com>
2022-02-23 21:51:35 -0500

    old commits

--- Labelling/config/constants.py
+++ Labelling/config/constants.py
@@ -1,7 +1,7 @@
 """ Welcome to the home of the constants. Configure these as you will. """
 
 TEST_IMG_FILE_PATH: str = ""
-TEST_IMG_DIR_PATH: str = ""
-AUTHOR: str = ""
+TEST_IMG_DIR_PATH: str = "C:\\Users\\ammar\\Documents\\CodingProjects\\ART\\ArtisticCV\\data"
+AUTHOR: str = "Ammar Vora"
 
 IMG_EXT: set = {'.jpg', '.jpeg', '.png'}
