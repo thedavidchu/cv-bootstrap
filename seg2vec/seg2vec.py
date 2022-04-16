64f994a7d8559aa222787fe42714d932b74c047d
David Chu <thedavidchu@gmail.com>
2022-04-16 13:20:29 -0400

    Laid out file structure of seg1vec.py. This is supposed to turn a segmentation label (assuming segmentations are linear) into a vector

---
+++ seg2vec/seg2vec.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+
+if __name__ == "__main__":
+    # Get label
+    # Linear fit each chunk as a line or
+    # Force it to group into k lines/quadratics
+    # Get and print label
+    # Print linear lines on top
+    # Print grouped lines/quadratics
+
+    pass
